db6a4da9b9afaeda53f8a4d258d0781887e46bbd
Slezin Dmitry <slezindimawow@gmail.com>
2023-11-29 13:54:18 +0300

    with error 3

--- Program.cs
+++ Program.cs
@@ -7,5 +7,6 @@ internal class Program
         Console.WriteLine("Test");
         Console.WriteLine()
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
     }
 }
